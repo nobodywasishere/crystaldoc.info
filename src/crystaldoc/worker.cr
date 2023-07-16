ac87b35c3f7f265a38e62c8e9c8045574b491682
Margret Riegert <margret@eowyn.net>
2023-07-16 18:24:15 -0400

    SQL query and version commit_id fixes

--- src/crystaldoc/worker.cr
+++ src/crystaldoc/worker.cr
@@ -6,22 +6,24 @@ module CrystalDoc
       directory = Path[directory].absolute? ? directory : PROJECT_ROOT / directory
 
       Process.run("firejail", [
-        "--noprofile",
-        "--read-only=#{PROJECT_ROOT}",
-        "--read-write=#{directory}",
-        "--restrict-namespaces",
-        "--rlimit-as=3g",
-        "--timeout=00:15:00",
-        cmd,
-        *args
-      ])
+          "--noprofile",
+          "--read-only=#{PROJECT_ROOT}",
+          "--read-write=#{directory}",
+          "--restrict-namespaces",
+          "--rlimit-as=3g",
+          "--timeout=00:15:00",
+          cmd,
+          *args,
+        ],
+        output: STDOUT, error: STDERR
+      )
     end
 
     def self.generate_docs(repo : CrystalDoc::Repo)
       temp_folder = Path["#{repo.username}-#{repo.project_name}"].expand
 
       # clone repo from site
-      unless Process.run("git", ["clone", repo.source_url, temp_folder.to_s]).success?
+      unless Process.run("git", ["clone", repo.source_url, temp_folder.to_s], output: STDOUT, error: STDERR).success?
         raise "Failed to clone URL: #{repo.source_url}"
       end
 
@@ -32,12 +34,11 @@ module CrystalDoc
 
       # cd into repo
       Dir.cd(temp_folder) do
-        # if repo.versions.empty?
-        #   repo.versions.add(`git describe --tags --abbrev=0`.strip)
-        # end
-
         repo.versions.each do |version|
-          `git checkout "#{version}"`
+          next if File.exists? "../public#{repo.path}/#{version.commit_id}"
+
+          p version
+          `git checkout "#{version.commit_id}"`
 
           # shards install to install dependencies
           # return an error if this fails
@@ -47,18 +48,20 @@ module CrystalDoc
 
           # generate docs using crystal
           # return an error if this fails
-          unless execute_firejail("crystal", ["doc", "--json-config-url=/#{repo.path}/versions.json"], temp_folder.to_s).success?
+          unless execute_firejail("crystal", ["doc", "--json-config-url=#{repo.path}/versions.json"], temp_folder.to_s).success?
             raise "Failed to generate documentation with Crystal"
           end
 
           # copy docs to `/public/:site/:repo/:user` folder
-          `mv "docs" "../public/#{repo.path}/#{version}"`
+          `mv "docs" "../public/#{repo.path}/#{version.commit_id}"`
+        rescue
+          puts "Failed to generate documentation for #{repo.path}"
         end
       end
 
       "Generated #{repo.path} documentation"
     ensure
-      # `rm -rf "#{temp_folder}"`
+      `rm -rf "#{temp_folder}"`
     end
   end
 end
