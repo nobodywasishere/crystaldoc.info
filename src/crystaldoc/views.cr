91e7004365490cca1faacda148e7b19720b5124c
Margret Riegert <margret@eowyn.net>
2023-07-16 23:16:01 -0400

    Move repo_list into sidebar view out of layout Force checkout branches

--- src/crystaldoc/views.cr
+++ src/crystaldoc/views.cr
@@ -3,6 +3,16 @@ require "ecr"
 module CrystalDoc
   module Views
     class Sidebar
+      getter repo_list : Array(CrystalDoc::Repo)
+
+      def initialize
+        @repo_list = DB.open(ENV["POSTGRES_DB"]) do |db|
+          db.transaction do |tx|
+            CrystalDoc::Queries.get_repos(tx.connection).sort_by{ |r| "#{r.username}/#{r.project_name}" }
+          end
+        end || [] of CrystalDoc::Repo
+      end
+
       ECR.def_to_s "./src/views/sidebar.ecr"
     end
   end
