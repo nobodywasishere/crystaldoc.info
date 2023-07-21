b735875b6c35ce05eb43d39dcee54dad29b46abc
Gwen Dowling <gwen_dowling@pm.me>
2023-07-21 14:36:38 -0400

    Implemented method for bulk html post processing (#3)

---
+++ src/crystaldoc/html.cr
@@ -0,0 +1,23 @@
+require "lexbor"
+
+module CrystalDoc
+  class Html
+    # Allows applying a single post processing block to all html files in a directory.
+    # Finds all html files in the specified path, opens and parses them with lexbor,
+    # then yields the lexbor representation of the file and its path to the block;
+    # when the block finishes, the changes are written back to the html file.
+    def self.post_process(path : String, &)
+      Dir.cd(path) do
+        Dir.glob("**/*.html").each do |file_path|
+          File.open(file_path, mode: "r+") do |file|
+            html = Lexbor::Parser.new(file)
+            yield html, file_path
+            modified = html.to_html
+            file.truncate
+            file.print modified
+          end
+        end
+      end
+    end
+  end
+end
