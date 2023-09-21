cac035ba56e0d9075cf2c3434796de6838588524
Margret Riegert <margret@eowyn.net>
2023-09-20 23:23:48 -0400

    Basic cli

---
+++ src/crystaldoc_cli.cr
@@ -0,0 +1,44 @@
+require "./crystaldoc"
+require "option_parser"
+
+REPO_DB = DB.open(ENV["POSTGRES_DB"])
+
+cmd = ""
+source = ""
+repo = ["", "", ""]
+version = ""
+
+OptionParser.parse do |parser|
+  parser.on "regenerate-all", "Regenerate all repo docs as doc jobs" do
+    cmd = "regenerate-all"
+  end
+
+  parser.on "regenerate", "Regenerate repo doc version" do
+    cmd = "regenerate"
+    parser.on("--repo=REPO", "Repo triad") { |s| repo = s.split("/") }
+    parser.on("--version=VERSION", "Repo git tag") { |w| version = w }
+    parser.on("--source=SOURCE_URL", "Repo git URL") { |t| source = t }
+  end
+  parser.on("-h", "--help", "Show this help") do
+    puts parser
+    exit
+  end
+end
+
+case cmd
+when "regenerate-all"
+  versions = REPO_DB.query_all(<<-SQL, as: Int32)
+    SELECT repo_version.id
+    FROM crystal_doc.repo_version;
+  SQL
+
+  versions.each do |version|
+    CrystalDoc::Queries.insert_doc_job(REPO_DB, version, 0)
+  end
+when "regenerate"
+  builder = CrystalDoc::Builder.new(source, repo[0], repo[1], repo[2], version)
+  builder.build
+else
+  puts "Unknown cmd #{cmd.inspect}"
+  exit 1
+end
